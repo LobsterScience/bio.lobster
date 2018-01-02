eed1f806ebfa6b078a9bae6f907b8aa0d490b8d4
BradHubley <brad.hubley@dfo-mpo.gc.ca>
2018-01-02 13:20:12 -0400

    working on the sim

--- R/pMat.R
+++ R/pMat.R
@@ -1,13 +1,15 @@
 #' @export
-pMat = function(p,cl){
+pMat = function(p,cl,lfa=NULL){
 
 
-	if(p$Area == "27N" || p$Area == "27S" || p$Area == "27")  lfa = "LFA27-30" # 27
-	if(p$Area == "29")                                        lfa = "LFA29" # 29
-	if(p$Area == "30")                                        lfa = "LFA28,30" # 30
-	if(p$Area == "31A")                                       lfa = "LFA29" # 31A
-	if(p$Area == "31B" || p$Area == "32")                     lfa = "LFA32" # 31B & 32
-	if(p$Area == "33E" || p$Area == "33W" || p$Area == "33")  lfa = "LFA33" # 33
+	if(is.null(lfa)){
+		if(p$Area == "27N" || p$Area == "27S" || p$Area == "27")  lfa = "LFA27-30" # 27
+		if(p$Area == "29")                                        lfa = "LFA29" # 29
+		if(p$Area == "30")                                        lfa = "LFA28,30" # 30
+		if(p$Area == "31A")                                       lfa = "LFA29" # 31A
+		if(p$Area == "31B" || p$Area == "32")                     lfa = "LFA32" # 31B & 32
+		if(p$Area == "33E" || p$Area == "33W" || p$Area == "33")  lfa = "LFA33" # 33
+	}
 
 	pl = data.frame(LFA=c("LFA27-30","LFA29","LFA28,30","LFA31A","LFA32x","LFA32","LFA33x","LFA33","LFA34"),
 		a=c(14.266, 14.173, 16.505, 14.53521, 10.4, 18.99223, 14.23,24.87275,22.37302),
