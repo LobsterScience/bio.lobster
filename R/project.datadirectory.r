388055589a0a34d2efddaaf733d9deb0a6c925b8
amcook <amcook127@gmail.com>
2020-06-18 11:28:58 -0300

    fixing exports

--- R/project.datadirectory.r
+++ R/project.datadirectory.r
@@ -1,4 +1,4 @@
-
+#' @export
 
 project.datadirectory = function(..., alternate.directory=NULL) {
   
--- R/project.figuredirectory.r
+++ R/project.figuredirectory.r
@@ -1,4 +1,4 @@
-
+#' @export
 
 project.figuredirectory = function(..., alternate.directory=NULL) {
   
