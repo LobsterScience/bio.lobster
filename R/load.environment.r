d2de7605bd39892de0e7a545203af750fa3146b0
amcook <amcook127@gmail.com>
2020-06-18 09:22:14 -0300

    remove import

--- R/load.environment.r
+++ R/load.environment.r
@@ -4,12 +4,12 @@ load.environment <- function(){
    p = list()
     p$project.name = "bio.lobster"
 
-    p$libs = RLibrary( c('RODBC', 'rgdal',"devtools","roxygen2","geosphere", "chron","fields",  "parallel","sp","spatstat","PBSmapping","RColorBrewer" ,'lattice','MASS','doBy','bio.lobster','bio.utilities','bio.lobster','bio.survey',"lubridate","SpatialHub"))
+#    p$libs = RLibrary( c('RODBC', 'rgdal',"devtools","roxygen2","geosphere", "chron","fields",  "parallel","sp","spatstat","PBSmapping","RColorBrewer" ,'lattice','MASS','doBy','bio.lobster','bio.utilities','bio.lobster','bio.survey',"lubridate","SpatialHub"))
       p$current.assessment.year=year(Sys.time())
 #load_all('~/git/bio.spacetime')
       p$lfas=c("27", "28", "29", "30", "31.1", "31.2", "32", "33", "34", "35", "36", "37", "38")
       p$spatial.domain = "canada.east"  # for temperature/habitat lookups
-      p = spatial_parameters(p=p)
+      p = bio.lobster::spatial_parameters(p=p)
     
 	  p$nw=10       #for temperature merge
 return(p)
