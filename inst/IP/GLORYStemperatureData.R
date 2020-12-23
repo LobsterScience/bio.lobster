485384f00a1783ee597f696b74601f0cd8503462
amcook <amcook127@gmail.com>
2020-12-23 11:30:35 -0400

    glorys updating

--- inst/IP/GLORYStemperatureData.R
+++ inst/IP/GLORYStemperatureData.R
@@ -74,9 +74,14 @@ image(a@lon, a@lat, t(a@data[,,1,]))
 load(file='/SpinDr/backup/bio_data/bio.lobster/data/maps/LFA27-33100mIsobath.rdata') #Isob100 the 100 m isobath for 27-33
 
 
+fil = dir()
+fil = fil[grep('GLO',fil)]
 
-glorysSubset
+for(i in 1:length(fil)){
+		g = glorysSubset(glorysfile=fil[i], polygon=Isob100)
+		saveRDS(g,file = file.path('SummaryFiles',paste(fil[i],'Isobath100.rds',sep="_")))
+	}
 
 
 
-
+#error with 2018
