9d9dd63fac5f602d5bc8ae5857c672ba4dd261aa
jakeelement <74512300+jakeelement@users.noreply.github.com>
2021-01-28 10:41:33 -0400

    Marport stuff removed (now in ILTS.sensor)

--- inst/OldPotentiallyDepricatedCode/5.ILTSMarportSummary.r
+++ inst/OldPotentiallyDepricatedCode/5.ILTSMarportSummary.r
@@ -1,10 +1,20 @@
+
+#### this script reads the marport text files, extracts useful data and converts this into a workable dataframe. 
+#### For converting this ouput to Esonar format, use the Marport2Esonar script.
+
 options(stringsAsFactors=F)
 require(bio.lobster)
 require(bio.utilities)
-fpath = file.path(project.datadirectory('bio.lobster'),'data','survey','2015')
+require(devtools)
+load_all(file.path(code_root,"bio.lobster"))
+fpath = file.path(project.datadirectory('bio.lobster'),'data','survey')
 
-marpath = file.path(fpath,'marport')
-tpath = file.path(fpath,'temperature')
+##### choose year of marport data to be summarized here. Adjust file paths if folder structure has changed.:
+year = 2015
+
+if(year == 2015){marpath= file.path(fpath,'2015','marport')}
+if(year == 2016){marpath= file.path(fpath,'ILTS_2016_Marport')}
+
 
 fm = dir(marpath,full.names=T)
 
@@ -21,7 +31,17 @@ for(i in fm) {
  	
 }
 
+
+
 gps = do.call(rbind,gps)
 sensors = do.call(rbind,sensors)
-save(sensors, file=file.path(project.datadirectory('bio.lobster'),'data','survey','marport.rdata'))
-save(gps, file=file.path(project.datadirectory('bio.lobster'),'data','survey','marport.gps.rdata'))
+
+####### output is to survey folder
+save(sensors, file=file.path(project.datadirectory('bio.lobster'),'data','survey',paste0('marport.',year,'.rdata.')))
+save(gps, file=file.path(project.datadirectory('bio.lobster'),'data','survey',paste0('marport.gps.',year,'.rdata.')))
+
+
+##### To convert output to Esonar format, run the following:
+
+marport2esonar(year=year)
+
