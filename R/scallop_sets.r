2138aab59b03269278888e9a2b64f6c5ddc46398
Cook <Adam.Cook@dfo-mpo.gc.ca>
2023-01-25 10:42:05 -0400

    upload sensor data

---
+++ R/scallop_sets.r
@@ -0,0 +1,26 @@
+#' @export
+
+scallop_sets <- function(){
+    x= read.csv(file.path(project.datadirectory('bio.lobster'),'data','ScallopSurvey','berried_lobster_bycatch_Jan17_23.csv'))
+    x$LONGITUDE = convert.dd.dddd(apply(x[,c('START_LONG','END_LONG')],1,FUN=mean))
+    x$LATITUDE =  convert.dd.dddd(apply(x[,c('START_LAT','END_LAT')],1,FUN=mean))
+    x$X1 = convert.dd.dddd(x[,c('START_LONG')])
+    x$Y1 = convert.dd.dddd(x[,c('START_LAT')])
+    x$X2 = convert.dd.dddd(x[,c('END_LONG')])
+    x$Y2 = convert.dd.dddd(x[,c('END_LAT')])
+    gear_width= 5.4864 #18ft A Glass
+    x$OFFSET = apply(x,1,function(y) geosphere::distm(c(y['X1'],y['Y1']),c(y['X2'],y['Y2'])))/1000 * gear_width/1000
+    x$SOURCE = 'Scallop survey'
+    x$OFFSET_METRIC = 'dist x spread km2'
+    x$DATE = as.Date(x$TOW_DATE)
+    x$YEAR = year(x$DATE)
+    x$DYEAR = lubridate::yday(x$DATE)/365
+    x$Lobster = x$TOTAL_LOBSTERS
+    x$Berried = x$TOTAL_BERRIED
+    x$Legal = x$WEIGHT_KG=NA
+    bigO = quantile(x$OFFSET,0.99)
+    littleO = quantile(x$OFFSET,0.01)
+    
+    x = subset(x,littleO<OFFSET & OFFSET<bigO,select=c(DATE, YEAR, LONGITUDE, LATITUDE,WEIGHT_KG, OFFSET, SOURCE, OFFSET_METRIC,DYEAR,Lobster, Berried,Legal))
+  return(x)
+    }
---
+++ R/uploadSensors.r
@@ -0,0 +1,37 @@
+#' @export
+uploadSenors <- function(datafile,tablenm, appendIt=F){
+        Sys.setenv(TZ = "GMT")
+        Sys.setenv(ORA_SDTZ = "GMT")   
+   if(grep('.csv',datafile)) datafile = read.csv(datafile,header=T)
+      if(dim(datafile)[2]!=19) stop('The number of columns in the datafile do not match the number required (19)')  
+      bio.lobster::db.setup()  
+      datafile$GPSDATE = as.Date(datafile$GPSDATE)
+      if(appendIT=F & ROracle::dbExistsTable(con, tablenm)) stop('table already exists in the space. You need to either use a new name or use appendIT=T')
+      if(appendIT=F & !ROracle::dbExistsTable(con, tablenm))){    
+                        dbSendQuery(conn=con, statement = paste("create table ",tablenm,"(",
+                                              "CPUDATEANDTIME VARCHAR2(26BYTE),
+                                               GPSTIME VARCHAR2(26BYTE),
+                                               LATITUDE VARCHAR2(14BYTE),
+                                               LONGITUDE VARCHAR2(15BYTE),
+                                               SPEED NUMBER(3,1),
+                                               HEADING NUMBER(3,0),
+                                               VALIDITY VARCHAR2(10BYTE),
+                                               TRANSDUCERNAME VARCHAR2(26BYTE),
+                                               SENSORNAME VARCHAR2(26BYTE),
+                                               SENSORVALUE NUMBER(11,6),
+                                               ERRORCODE VARCHAR2(26BYTE),
+                                               HYDROPHONE VARCHAR2(26BYTE),
+                                               SET_NO NUMBER(3,0),
+                                               TRIP_ID NUMBER(10,0),
+                                               GPSDATE DATE,
+                                               DDLAT NUMBER(10,0),
+                                               DDLON NUMBER(10,0),
+                                               SOURCE VARCHAR2(20BYTE),
+                                               SIGNALSTRENGTH NUMBER)",SEP=" ")
+                        )
+        print(paste('Table ',tablenm,' has been created',sep=""))
+      }
+    dbWriteTable(conn=con,name=tablenm,append=appendIt, value=datafile)
+    print('fileUploaded')
+
+}
