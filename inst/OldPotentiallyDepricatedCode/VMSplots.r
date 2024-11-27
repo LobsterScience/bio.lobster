20f0a835bf1c06753160d845650689d8c69bf9fa
Cook <Adam.Cook@dfo-mpo.gc.ca>
2024-11-27 14:29:50 -0400

    update VMS

--- inst/OldPotentiallyDepricatedCode/VMSplots.r
+++ inst/OldPotentiallyDepricatedCode/VMSplots.r
@@ -1,7 +1,7 @@
 #VMS_plots
 
 
-logs41   = lobster.db('logs41')
+lobster.db('logs41')
 vms.data = lobster.db('lfa41.vms')
 logs41$YEAR = year(logs41$FV_FISHED_DATETIME)
 
@@ -9,17 +9,17 @@ logs41$YEAR = year(logs41$FV_FISHED_DATETIME)
   logs41 = makePBS(logs41,polygon=F)
 
   logs41 = logs41[-which(is.na(logs41[,c('X','Y','EID')])),]
-
+  logs41$X = logs41$X*-1
 yr = unique(logs41$YEAR)
 yrv = unique(vms.data$YEAR)
 yr = intersect(yr,yrv)
 
-pdf(file=file.path(project.figuredirectory('lobster'),'lfa41LogsVMS.pdf'))
+pdf(file=file.path(project.figuredirectory('bio.lobster'),'lfa41LogsVMS.pdf'))
 for(y in yr) {
 
 LobsterMap('41')
 title(y)
 addPoints(subset(vms.data,YEAR==y),pch='.',col='red')
-addPoints(subset(logs41,YEAR==y),pch='.',col='green')
+addPoints(subset(logs41,YEAR==y & !is.na(X),select=c(X,Y,EID)),pch='.',col='green')
 }
 dev.off()
